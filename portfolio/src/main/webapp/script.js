bd5b5924e4194eadd2611e22cabefd616108a6a8
Ethan Rigby <ethanrigby@live.com>
2020-06-08 10:27:42 -0400

    Fix changes to be made.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,15 +26,3 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
-
-// function addBackground()  {
-//     const info = document.getElementById('info-container');
-//     info.innerText = "Hello, my name is Ethan Rigby and I am  a 4th year student at the University of Calgary.\n
-//     I don't have many pictures on my laptop but feel free to take a look at my instagram as I like taking pictures of \n
-//     flowers and the places I have been. I usually like to take macro pictures of small things like bugs and flowers. \n
-//     I also love to spend time with my family when I am home since I go to school halfway across the country (around a 40 hour \n
-//     car ride to be exact). I also love to snowboard and do a lot of teaching in my spare time. I go out to the mountains often\n
-//     and volunteer with a group called the Canadian Adaptive Snowsports (CADS). This lets me help teach other people who have\n
-//     disabilities how to snowboard. I have been working with someone for the past three years and we finally got him on and off\n
-//     the chair lift successfully this year! I also coach someone who is visually impared and is striving to go to the paralympics!";
-// }
